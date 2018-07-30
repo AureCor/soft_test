fabafb1f0d4c6c0c2229c0b99596c97cf4e3f344
Aurelio Cortese <13802882+Aureee@users.noreply.github.com>
2018-07-30 16:38:48 -0700

    adding more files

--- demean_age.py
+++ demean_age.py
@@ -1,9 +1,23 @@
+# read data by importing from csv file
+#import numpy as np
+#
+#age = np.loadtxt("participants.tsv", skiprows=1, usecols=3)
+#
+#mean_age = sum(age)/len(age)
+#
+#np.savetxt("demeaned_age.txt", age-mean_age)
+#
+#print("done!")
+
+
+# read data directly from command line
+import sys
 import numpy as np
 
-age = np.loadtxt("participants.tsv", skiprows=1, usecols=3)
+age = np.loadtxt(sys.argv[1], skiprows=1, usecols=3)
 
 mean_age = sum(age)/len(age)
 
-np.savetxt("demeaned_age.txt", age-mean_age)
+np.savetxt("demeaned_" + sys.argv[1], age-mean_age)
 
 print("done!")
