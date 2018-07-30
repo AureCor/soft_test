89eca7da50bbc617649ce01f9122f51ef4388adc
Aurelio Cortese <13802882+Aureee@users.noreply.github.com>
2018-07-30 16:56:24 -0700

    break

--- demean_age.py
+++ demean_age.py
@@ -13,10 +13,12 @@
 # read data directly from command line
 import sys
 import numpy as np
+import pandas as pd
 
-age = np.loadtxt(sys.argv[1], skiprows=1, usecols=2)
+#age = np.loadtxt(sys.argv[1], skiprows=1, usecols=1)
+df = pd.read_csv(sys.argv[1],sep='\t')
 
-mean_age = sum(age)/len(age)
+mean_age = sum(df.age)/len(df.age)
 assert mean_age < 100
 assert mean_age > 10
 
