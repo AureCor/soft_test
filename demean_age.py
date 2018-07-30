9c6adf613a472c6a1671f5681cfe17015980b2da
Aurelio Cortese <13802882+Aureee@users.noreply.github.com>
2018-07-30 16:48:06 -0700

    break

--- demean_age.py
+++ demean_age.py
@@ -17,10 +17,10 @@ import numpy as np
 age = np.loadtxt(sys.argv[1], skiprows=1, usecols=3)
 
 mean_age = sum(age)/len(age)
+assert mean_age < 100
+assert mean_age > 30
 
 np.savetxt("demeaned_" + sys.argv[1], age-mean_age)
 
-assert mean_age < 100
-assert mean_age > 30
 
 print("done!")
