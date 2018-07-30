3175cce982c7dd21bfbc34fc8c4fc712fdbcdfaa
Aurelio Cortese <13802882+Aureee@users.noreply.github.com>
2018-07-30 16:58:37 -0700

    break

--- demean_age.py
+++ demean_age.py
@@ -22,7 +22,7 @@ mean_age = df.age.mean()
 assert mean_age < 100
 assert mean_age > 10
 
-np.savetxt("demeaned_" + sys.argv[1], age-mean_age)
+np.savetxt("demeaned_" + sys.argv[1], df.age-mean_age)
 
 
 print("done!")
